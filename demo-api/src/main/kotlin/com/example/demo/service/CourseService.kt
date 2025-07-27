cfce70b46788bfec2bce96c45c398017070ed17f
약풍 <yakpoong@inflab.com>
2025-07-27 14:04:31 +0900

    chore: 에러로그 추가

--- demo-api/src/main/kotlin/com/example/demo/service/CourseService.kt
+++ demo-api/src/main/kotlin/com/example/demo/service/CourseService.kt
@@ -16,6 +16,17 @@ class CourseService(
          * findBySubInstructorId-O1HwSwE(long) : java.util.List
          *
          * 런타임에서는 long 타입으로 파라미터가 들어오는데, Entity 파일에는 Id<Course> 타입으로 인식되어 에러 발생
+         *
+         * Argument [3] of type [java.lang.Long] did not match parameter type [com.example.demo.Id (n/a)]
+         * org.hibernate.query.QueryArgumentException: Argument [3] of type [java.lang.Long] did not match parameter type [com.example.demo.Id (n/a)]
+         * 	at app//org.hibernate.query.spi.QueryParameterBindingValidator.validate(QueryParameterBindingValidator.java:85)
+         * 	at app//org.hibernate.query.spi.QueryParameterBindingValidator.validate(QueryParameterBindingValidator.java:32)
+         * 	at app//org.hibernate.query.internal.QueryParameterBindingImpl.validate(QueryParameterBindingImpl.java:366)
+         * 	at app//org.hibernate.query.internal.QueryParameterBindingImpl.setBindValue(QueryParameterBindingImpl.java:142)
+         * 	at app//org.hibernate.query.spi.AbstractCommonQueryContract.setParameter(AbstractCommonQueryContract.java:1045)
+         * 	at app//org.hibernate.query.spi.AbstractSelectionQuery.setParameter(AbstractSelectionQuery.java:668)
+         * 	at app//org.hibernate.query.sqm.internal.QuerySqmImpl.setParameter(QuerySqmImpl.java:1127)
+         *
          */
         courseApiRepository::class.java.methods
             .filter { it.name.startsWith("findBySubInstructorId") }
